78ecb6db5df9a5c1f117f990f32822cd39642e17
Mohamed Elalfy <mohamedelalfy378@gmail.com>
2026-08-12 17:46:05 +0300

    Remove TestScreen widget and replace gomeScreen with Placeholder in PersistenBottomNavBarDemo

--- lib/core/shared/app_widgets/bottom_navigation_bar.dart
+++ lib/core/shared/app_widgets/bottom_navigation_bar.dart
@@ -1,4 +1,3 @@
-import 'package:flower_app/core/shared/app_widgets/test_screen.dart';
 import 'package:flower_app/core/themes/app_colors/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
@@ -7,7 +6,7 @@ import '../../constants/app_strings/app_strings.dart';
 
 class PersistenBottomNavBarDemo extends StatelessWidget {
    PersistenBottomNavBarDemo({super.key});
-  final Widget gomeScreen = TestScreen();
+  final Widget gomeScreen = Placeholder();
   final Widget categoriesScreen = Placeholder();
   final Widget cartScreen = Placeholder();
   final Widget profileScreen2 = Placeholder();
