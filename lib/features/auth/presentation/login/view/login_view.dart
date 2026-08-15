9b2d03aafce356babb5b2dda2422f25c91e0fe2f
beso <bassionymohamed898@gmail.com>
2026-08-15 07:06:04 +0300

    Enable auto-validation for the login form on user interaction

--- lib/features/auth/presentation/login/view/login_view.dart
+++ lib/features/auth/presentation/login/view/login_view.dart
@@ -94,6 +94,7 @@ class _LoginViewState extends State<LoginView> {
               padding: const EdgeInsets.all(24),
               child: Form(
                 key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
