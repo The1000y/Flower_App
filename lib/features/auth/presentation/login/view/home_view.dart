3b62c1d90a20a57f1c0d5c9b71e8364f614b24c7
beso <bassionymohamed898@gmail.com>
2026-08-16 15:56:30 +0300

    route and navigate manulally to test

---
+++ lib/features/auth/presentation/login/view/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: 
+      Center(
+        child: Row(
+          children: [
+            Text("comming soon"),
+            CircularProgressIndicator()
+          ],
+        ),
+      )),
+    );
+  }
+}
