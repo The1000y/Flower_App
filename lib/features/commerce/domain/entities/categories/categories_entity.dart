793753aa6daa5e7b343bac27e4146c4be64e020f
mo-david <momodawoud.2299@gmail.com>
2026-09-03 13:05:06 +0300

    conflict resolved

---
+++ lib/features/commerce/domain/entities/categories/categories_entity.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class CategoryEntity extends Equatable {
+  final int id;
+  final String name;
+  final String iconUrl;
+
+  const CategoryEntity({
+    required this.id,
+    required this.name,
+    required this.iconUrl,
+  });
+
+  @override
+  List<Object?> get props => [id, name, iconUrl];
+}
