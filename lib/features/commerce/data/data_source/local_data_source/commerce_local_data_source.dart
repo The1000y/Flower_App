4f30cd150c103f1bc279d6466397e153474d11dc
Ahmed Nasser <ahmed.devol123@gmail.com>
2026-09-05 11:01:59 +0300

    implement add cart

--- lib/features/commerce/data/data_source/local_data_source/commerce_local_data_source.dart
+++ lib/features/commerce/data/data_source/local_data_source/commerce_local_data_source.dart
@@ -1,8 +1,5 @@
 import 'package:flower_app/config/base/base_responce.dart';
-import 'package:flower_app/features/commerce/data/model/request/cart_request/add_cart_item_request_dto.dart';
-import 'package:flower_app/features/commerce/data/model/request/cart_request/update_cart_item_request_dto.dart';
 import 'package:flower_app/features/commerce/data/model/responce/best_seller/item_Dto.dart';
-import 'package:flower_app/features/commerce/data/model/responce/cart_response/cart_response_dto.dart';
 import 'package:flower_app/features/commerce/data/model/responce/categories_response/category_dto.dart';
 import 'package:flower_app/features/commerce/data/model/responce/home_response/section_dto.dart';
 import 'package:flower_app/features/commerce/data/model/responce/occasion_response/occasion_dto.dart';
@@ -19,4 +16,10 @@ abstract interface class CommerceLocalDataSource {
   Future<BaseResponce<List<ItemDto>>> getBestSellers();
   Future<BaseResponce<List<SectionDto>>> getSections();
   Future<BaseResponce<List<OccasionDto>>> getOccasions();
+  Future<BaseResponce<CartResponseDto>> getCart();
+  Future<BaseResponce<CartResponseDto>> addToCart(AddCartItemRequestDto request);
+Future<BaseResponce<CartResponseDto>> updateCartItemQuantity(
+  String cartItemId,
+  UpdateCartItemRequestDto request,
+);  Future<BaseResponce<CartResponseDto>> removeCartItem(String cartItemId);
 }
