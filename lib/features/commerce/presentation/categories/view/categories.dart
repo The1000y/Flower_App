e97de85a0c2e12dcc0db6fef6837e1eed06ea5c0
beso <bassionymohamed898@gmail.com>
2026-09-07 23:48:10 +0300

    Add missing import for custom text form field and include product ID in search result

--- lib/features/commerce/presentation/categories/view/categories.dart
+++ lib/features/commerce/presentation/categories/view/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flower_app/core/constants/app_strings/app_strings.dart';
+import 'package:flower_app/core/shared/app_widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -148,7 +148,7 @@ class _SearchViewState extends State<SearchView> {
             name: product.name,
             price: product.price,
             oldPrice: product.originalPrice,
-            discount: product.discountPercentage?.round(),
+            discount: product.discountPercentage?.round(), id: product.id,
           ),
         );
       },
