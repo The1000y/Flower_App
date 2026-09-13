55582f21c8675a726f3ead5aeb3c8c07bf2b8eab
beso <bassionymohamed898@gmail.com>
2026-09-13 19:13:33 +0300

    feat: Implement change password functionality
    - Added API endpoint for changing password in ApiStrings.
    - Updated AppStrings to include success message for password update.
    - Created ProfileApiClient for handling change password requests.
    - Implemented ProfileLocalDataSource and ProfileRemoteDataSource for change password.
    - Developed ChangePasswordRequest and ChangePasswordResponse models.
    - Created ChangePasswordEntity for domain representation.
    - Implemented ProfileRepo for managing change password logic.
    - Added ChangePasswordUseCase for encapsulating change password use case.
    - Developed ChangePasswordCubit for managing state in the UI.
    - Created ChangePasswordView for user interface to change password.
    - Added tests for all new functionalities including API client, data sources, repository, use case, cubit, and view.

--- lib/features/profile/data/data_source/remote_data_source/profile_remote_data_source.dart
+++ lib/features/profile/data/data_source/remote_data_source/profile_remote_data_source.dart
@@ -1,7 +1,11 @@
 import 'package:flower_app/config/base/base_responce.dart';
 import 'package:flower_app/features/profile/data/model/request/change_password_request/change_password_request.dart';
+import 'package:flower_app/features/profile/data/model/request/update_profile_request_dto.dart';
 import 'package:flower_app/features/profile/data/model/response/change_password_response/change_password_response.dart';
+import 'package:flower_app/features/profile/data/model/response/get_profile_response_dto.dart';
 
 abstract interface class ProfileRemoteDataSource {
   Future<BaseResponce<ChangePasswordResponse>> changePassword(ChangePasswordRequest request);
+  Future<BaseResponce<GetProfileResponseDto>> getProfile();
+  Future<BaseResponce<GetProfileResponseDto>> updateProfile(UpdateProfileRequestDto request);
 }
